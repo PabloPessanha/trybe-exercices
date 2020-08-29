c10327c52cf797fccbb22c41388881e4a1063711
Pablo Pessanha <pablopessanh@gmail.com>
2020-08-29 19:38:23 -0300

    Starting the 5th module

--- 4th-module/15-exercises/ex6.js
+++
@@ -1,8 +0,0 @@
-var firstNumber = 15;
-var secondNumber = 10;
-
-if (firstNumber > secondNumber) {
-   console.log(firstNumber, "is bigger than", secondNumber);
-} else {
-   console.log(secondNumber, "is bigger than", firstNumber);
-}
--- 4th-module/15-exercises/ex9.js
+++
@@ -1,8 +0,0 @@
-function checkingTrinagule (a, b, c){
-   if (a + b + c >= 180){
-      return true;
-   } else {
-      return false;
-   }
-}
-console.log(checkingTrinagule(50, 80, 50));
