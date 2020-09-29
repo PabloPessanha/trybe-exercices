714479f7d92ea30047d3c190ed1ad0f5f9bc0340
Pablo Pessanha <pablopessanh@gmail.com>
2020-09-29 15:32:14 -0300

    2nd exercises of unit tests done

--- block-07/javascript-unit-tests/script.js
+++ block-07/javascript-unit-tests/script.js
@@ -17,3 +17,22 @@ assert.throws(() => {
 assert.throws(() => {
   sum(4, '5');
 }, /^Error: parameters must be numbers$/);
+
+// 2
+function myRemove(arr, item) {
+  let newArr = [];
+  for (let i = 0; i < arr.length; i += 1) {
+    if (item !== arr[i]) {
+      newArr.push(arr[i]);
+    }
+  }
+  return newArr;
+}
+
+// implemente seus testes aqui
+const arrayValue = [1, 2, 3, 4];
+const arrayNewValue = myRemove(arrayValue, 3);
+assert.deepStrictEqual(arrayNewValue, [1, 2, 4]);
+assert.notDeepStrictEqual(arrayNewValue, [1, 2, 3, 4]);
+assert.deepStrictEqual(arrayValue, [1, 2, 3, 4]);
+assert.deepStrictEqual(myRemove(arrayValue, 5), [1, 2, 3, 4]);
