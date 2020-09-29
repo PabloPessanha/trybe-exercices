27de01bcd76c4164b617e1aac68e099b44c1b64b
Pablo Pessanha <pablopessanh@gmail.com>
2020-09-29 16:27:11 -0300

    2nd exercise of practicing TDD done

--- block-07/javascript-unit-tests/practicing-tdd.js
+++ block-07/javascript-unit-tests/practicing-tdd.js
@@ -1,19 +1,40 @@
 const assert = require('assert');
 // 1
 // escreva a função addOne aqui
-function addOne(collection) {
-  const arrays = collection;
-  const newArray = [];
-  arrays.forEach((array) => {
-    newArray.push(array + 1);
-  });
-  return newArray;
-}
-const myArray = [31, 57, 12, 5];
-const unchanged = [31, 57, 12, 5];
-const expected = [32, 58, 13, 6];
-const output = addOne(myArray);
+{
+  function addOne(collection) {
+    const arrays = collection;
+    const newArray = [];
+    arrays.forEach((array) => {
+      newArray.push(array + 1);
+    });
+    return newArray;
+  }
+  const myArray = [31, 57, 12, 5];
+  const unchanged = [31, 57, 12, 5];
+  const expected = [32, 58, 13, 6];
+  const output = addOne(myArray);
 
-assert.strictEqual(typeof addOne, 'function');
-assert.deepStrictEqual(output, expected);
-assert.deepStrictEqual(myArray, unchanged);
+  assert.strictEqual(typeof addOne, 'function');
+  assert.deepStrictEqual(output, expected);
+  assert.deepStrictEqual(myArray, unchanged);
+}
+// 2
+// escreva a função wordLengths aqui
+{
+  function wordLengths(sentence) {
+    const arrays = sentence;
+    const newArray = [];
+    arrays.forEach((array) => {
+      newArray.push(array.length);
+    });
+    return newArray;
+  }
+
+  const words = ['sun', 'potato', 'roundabout', 'pizza'];
+  const expected = [3, 6, 10, 5];
+
+  assert.strictEqual(typeof wordLengths, 'function');
+  const output = wordLengths(words);
+  assert.deepStrictEqual(output, expected);
+}
