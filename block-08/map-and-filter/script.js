83d3c8e7f64bdb21da4437d7fa2a82fb17632aad
Pablo Pessanha <pablopessanh@gmail.com>
2020-10-01 20:00:27 -0300

    3rd exercise done

--- block-08/map-and-filter/script.js
+++ block-08/map-and-filter/script.js
@@ -63,6 +63,7 @@ const books = [
   },
 ];
 
+// 1
 const expected_result_formated = [
   'As Crônicas de Gelo e Fogo - Fantasia - George R. R. Martin',
   'O Senhor dos Anéis - Fantasia - J. R. R. Tolkien',
@@ -77,6 +78,7 @@ function formatedBookNames() {
   return formatedBooks;
 }
 
+// 2
 const expected_result_author_age = [
   {
     age: 31,
@@ -115,5 +117,54 @@ function nameAndAge() {
   return nameWithAge;
 }
 
+// 3
+const expected_result_genre = [
+  {
+    id: 1,
+    name: 'As Crônicas de Gelo e Fogo',
+    genre: 'Fantasia',
+    author: {
+      name: 'George R. R. Martin',
+      birthYear: 1948,
+    },
+    releaseYear: 1991,
+  },
+  {
+    id: 2,
+    name: 'O Senhor dos Anéis',
+    genre: 'Fantasia',
+    author: {
+      name: 'J. R. R. Tolkien',
+      birthYear: 1892,
+    },
+    releaseYear: 1954,
+  },
+  {
+    id: 3,
+    name: 'Fundação',
+    genre: 'Ficção Científica',
+    author: {
+      name: 'Isaac Asimov',
+      birthYear: 1920,
+    },
+    releaseYear: 1951,
+  },
+  {
+    id: 4,
+    name: 'Duna',
+    genre: 'Ficção Científica',
+    author: {
+      name: 'Frank Herbert',
+      birthYear: 1920,
+    },
+    releaseYear: 1965,
+  },
+];
+
+function fantasyOrScienceFiction() {
+  return books.filter((book) => book.genre === 'Fantasia' || book.genre === 'Ficção Científica');
+}
+
+assert.deepStrictEqual(fantasyOrScienceFiction(), expected_result_genre);
 assert.deepStrictEqual(nameAndAge(), expected_result_author_age);
 assert.deepStrictEqual(formatedBookNames(), expected_result_formated);
