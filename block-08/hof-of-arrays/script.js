077f816f5c1ea034c8b2a7fbec380da1acf16694
Pablo Pessanha <pablopessanh@gmail.com>
2020-10-01 11:58:22 -0300

    4th exercise done

--- block-08/hof-of-arrays/script.js
+++ block-08/hof-of-arrays/script.js
@@ -96,6 +96,67 @@ function getNamedBook() {
   return greatestBook;
 }
 
+// 4
+const expected_result_order = [
+  {
+    id: 1,
+    name: 'As Crônicas de Gelo e Fogo',
+    genre: 'Fantasia',
+    author: { name: 'George R. R. Martin', birthYear: 1948 },
+    releaseYear: 1991,
+  },
+  {
+    id: 5,
+    name: 'A Coisa',
+    genre: 'Terror',
+    author: { name: 'Stephen King', birthYear: 1947 },
+    releaseYear: 1986,
+  },
+  {
+    id: 4,
+    name: 'Duna',
+    genre: 'Ficção Científica',
+    author: { name: 'Frank Herbert', birthYear: 1920 },
+    releaseYear: 1965,
+  },
+  {
+    id: 2,
+    name: 'O Senhor dos Anéis',
+    genre: 'Fantasia',
+    author: { name: 'J. R. R. Tolkien', birthYear: 1892 },
+    releaseYear: 1954,
+  },
+  {
+    id: 3,
+    name: 'Fundação',
+    genre: 'Ficção Científica',
+    author: { name: 'Isaac Asimov', birthYear: 1920 },
+    releaseYear: 1951,
+  },
+  {
+    id: 6,
+    name: 'O Chamado de Cthulhu',
+    genre: 'Terror',
+    author: { name: 'H. P. Lovecraft', birthYear: 1890 },
+    releaseYear: 1928,
+  },
+];
+
+function booksOrderedByReleaseYearDesc() {
+  const descendingBooks = [];
+  const descendingYears = [];
+  books.forEach((book) => {
+    descendingYears.push(book.releaseYear);
+  });
+  descendingYears.sort().reverse();
+  descendingYears.forEach((year) => {
+    const newPush = books.find((book) => book.releaseYear === year);
+    descendingBooks.push(newPush);
+  });
+  return descendingBooks;
+}
+
 assert.strictEqual(authorBornIn1947(), 'Stephen King');
 assert.strictEqual(smallerName(), 'Duna');
 assert.deepStrictEqual(getNamedBook(), expected_result);
+assert.deepStrictEqual(booksOrderedByReleaseYearDesc(), expected_result_order);
