1546fb33b401cec8fb33f4bb0ee3ffedb62e6268
Pablo Pessanha <pablopessanh@gmail.com>
2020-10-02 11:40:22 -0300

    2nd exercise done

--- block-08/reduce/script.js
+++ block-08/reduce/script.js
@@ -96,14 +96,87 @@ function bestDiscipline() {
   });
 }
 
-//1
+// 1
 const arrays = [['1', '2', '3'], [true], [4, 5, 6]];
+const expected_array = ['1', '2', '3', true, 4, 5, 6];
 
-// Solução encontrada na documentação -> https://mzl.la/3l8yka2
+// Solução encontrada na documentação -> https://mzl.la/3l8yka2 - 2 métodos diferentes para mesma solução
 function flatten() {
   // return arrays.flat();
-  return arrays.reduce((acc, value) => acc.concat(value), []);
+  return arrays.reduce((emptyArray, arrayValue) => emptyArray.concat(arrayValue), []);
 }
 
+// 2
+const books = [
+  {
+    id: 1,
+    name: 'As Crônicas de Gelo e Fogo',
+    genre: 'Fantasia',
+    author: {
+      name: 'George R. R. Martin',
+      birthYear: 1948,
+    },
+    releaseYear: 1991,
+  },
+  {
+    id: 2,
+    name: 'O Senhor dos Anéis',
+    genre: 'Fantasia',
+    author: {
+      name: 'J. R. R. Tolkien',
+      birthYear: 1892,
+    },
+    releaseYear: 1954,
+  },
+  {
+    id: 3,
+    name: 'Fundação',
+    genre: 'Ficção Científica',
+    author: {
+      name: 'Isaac Asimov',
+      birthYear: 1920,
+    },
+    releaseYear: 1951,
+  },
+  {
+    id: 4,
+    name: 'Duna',
+    genre: 'Ficção Científica',
+    author: {
+      name: 'Frank Herbert',
+      birthYear: 1920,
+    },
+    releaseYear: 1965,
+  },
+  {
+    id: 5,
+    name: 'A Coisa',
+    genre: 'Terror',
+    author: {
+      name: 'Stephen King',
+      birthYear: 1947,
+    },
+    releaseYear: 1986,
+  },
+  {
+    id: 6,
+    name: 'O Chamado de Cthulhu',
+    genre: 'Terror',
+    author: {
+      name: 'H. P. Lovecraft',
+      birthYear: 1890,
+    },
+    releaseYear: 1928,
+  },
+];
+const expected_names = `Nomes: George R. R. Martin, J. R. R. Tolkien, Isaac Asimov, Frank Herbert, Stephen King, H. P. Lovecraft.`;
+
+function allNames() {
+  return books.reduce((names, book) => {
+    if (book.id < books.length) return (names += ` ${book.author.name},`);
+    return (names += ` ${book.author.name}.`);
+  }, 'Nomes:');
+}
 assert.deepStrictEqual(bestDiscipline(), expected_disciplines);
-assert.deepStrictEqual(flatten(), ['1', '2', '3', true, 4, 5, 6]);
+assert.deepStrictEqual(flatten(), expected_array);
+assert.deepStrictEqual(allNames(), expected_names);
