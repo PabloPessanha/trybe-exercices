bb7a82cb5623d97cb170bcc9d174f46cccf22518
Pablo <pablopessanh@gmail>
2020-10-07 19:07:05 -0300

    exercise 7 done

--- block-08/destructuring-and-parameters/script.js
+++ block-08/destructuring-and-parameters/script.js
@@ -123,3 +123,31 @@ const toObject = (cars) => {
 assert.deepStrictEqual(toObject(palio), { name: 'Palio', brand: 'Fiat', year: 2019 });
 assert.deepStrictEqual(toObject(shelbyCobra), { name: 'Shelby Cobra', brand: 'Ford', year: 1963 });
 assert.deepStrictEqual(toObject(chiron), { name: 'Chiron', brand: 'Bugatti', year: 2016 });
+
+// 7
+const ships = [
+  {
+    name: 'Titanic',
+    length: 269.1,
+  },
+  {
+    name: 'Queen Mary 2',
+    length: 1132,
+    measurementUnit: 'feet',
+  },
+  {
+    name: 'Yamato',
+    length: 256,
+    measurementUnit: 'meters',
+  },
+];
+
+// escreva shipLength abaixo
+const shipLength = (ship) => {
+  const { name, length, measurementUnit = 'meters' } = ship;
+  return `${name} is ${length} ${measurementUnit} long`;
+};
+
+assert.strictEqual(shipLength(ships[0]), 'Titanic is 269.1 meters long');
+assert.strictEqual(shipLength(ships[1]), 'Queen Mary 2 is 1132 feet long');
+assert.strictEqual(shipLength(ships[2]), 'Yamato is 256 meters long');
