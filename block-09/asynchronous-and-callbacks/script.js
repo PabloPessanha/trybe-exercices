52328fb36aca509236f032b09eac4bc2b77352c8
Pablo <pablopessanh@gmail>
2020-10-21 19:08:47 -0300

    4-2 exercise done

--- block-09/asynchronous-and-callbacks/script.js
+++ block-09/asynchronous-and-callbacks/script.js
@@ -21,21 +21,28 @@ const getMarsTemperature = () => {
 };
 
 // crie a função sendMarsTemperature abaixo
-const sendMarsTemperature = (temperature) => {
+const sendMarsTemperature = (temperature, error) => {
   setTimeout(() => {
     if (!temperature)
       return console.log(`Mars temperature is: ${getMarsTemperature()} degree Celsius`);
-    return temperature(getMarsTemperature());
+    try {
+      return temperature(getMarsTemperature());
+    } catch {
+      return error("Parameter isn't a function");
+    }
   }, messageDelay());
 };
 // sendMarsTemperature(); // imprime "Mars temperature is: 20 degree Celsius", por exemplo
 
-//4-1
+//4 - 1
 const toFahrenheit = (degreeCelsius) => (degreeCelsius * 9) / 5 + 32;
 const temperatureInFahrenheit = (temperature) =>
   console.log(`It is currently ${toFahrenheit(temperature)}ºF at Mars`);
 const greet = (temperature) =>
   console.log(`Hi there! Curiosity here. Right now is ${temperature}ºC at Mars`);
 
-sendMarsTemperature(temperatureInFahrenheit); // imprime "It is currently 47ºF at Mars", por exemplo
-sendMarsTemperature(greet); // imprime "Hi there! Curiosity here. Right now is 53ºC at Mars", por exemplo
+//4 - 2
+const handleError = (errorReason) => console.log(`Error getting temperature: ${errorReason}`);
+
+sendMarsTemperature(temperatureInFahrenheit, handleError); // imprime "It is currently 47ºF at Mars", por exemplo
+sendMarsTemperature(greet, handleError); // imprime "Hi there! Curiosity here. Right now is 53ºC at Mars", por exemplo
