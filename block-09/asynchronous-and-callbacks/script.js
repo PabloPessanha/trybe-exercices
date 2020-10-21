7e89cdc8498048a55ea283a76b7748f92e68af17
Pablo <pablopessanh@gmail>
2020-10-21 19:03:13 -0300

    4-1 exercise done

--- block-09/asynchronous-and-callbacks/script.js
+++ block-09/asynchronous-and-callbacks/script.js
@@ -10,4 +10,32 @@ const getPlanet = () => {
   setTimeout(() => console.log('Returned planet: ', mars), 4000);
 };
 
-getPlanet(); // imprime Marte depois de 4 segundos
+// getPlanet(); // imprime Marte depois de 4 segundos
+
+// 4
+const messageDelay = () => Math.floor(Math.random() * 5000);
+
+const getMarsTemperature = () => {
+  const maxTemperature = 58;
+  return Math.floor(Math.random() * maxTemperature);
+};
+
+// crie a função sendMarsTemperature abaixo
+const sendMarsTemperature = (temperature) => {
+  setTimeout(() => {
+    if (!temperature)
+      return console.log(`Mars temperature is: ${getMarsTemperature()} degree Celsius`);
+    return temperature(getMarsTemperature());
+  }, messageDelay());
+};
+// sendMarsTemperature(); // imprime "Mars temperature is: 20 degree Celsius", por exemplo
+
+//4-1
+const toFahrenheit = (degreeCelsius) => (degreeCelsius * 9) / 5 + 32;
+const temperatureInFahrenheit = (temperature) =>
+  console.log(`It is currently ${toFahrenheit(temperature)}ºF at Mars`);
+const greet = (temperature) =>
+  console.log(`Hi there! Curiosity here. Right now is ${temperature}ºC at Mars`);
+
+sendMarsTemperature(temperatureInFahrenheit); // imprime "It is currently 47ºF at Mars", por exemplo
+sendMarsTemperature(greet); // imprime "Hi there! Curiosity here. Right now is 53ºC at Mars", por exemplo
