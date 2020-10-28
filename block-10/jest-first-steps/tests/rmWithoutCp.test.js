7ac886941dd3d0c8d832bc22a88f161a6c85bfae
Pablo <pablopessanh@gmail>
2020-10-28 15:33:03 -0300

    3rd jest tests requirement done

--- block-10/jest-first-steps/tests/rmWithoutCp.test.js
+++ block-10/jest-first-steps/tests/rmWithoutCp.test.js
@@ -1,10 +1,27 @@
 const func = require('../src/rmWithoutCp');
 const { myRemoveWithoutCopy } = func;
 
-/*
-A função myRemoveWithoutCopy(arr, item) recebe um array arr e retorna o próprio array sem o elemento item caso ele exista no array
-Verifique se a chamada myRemoveWithoutCopy([1, 2, 3, 4], 3) retorna o array esperado
-Verifique se a chamada myRemoveWithoutCopy([1, 2, 3, 4], 3) não retorna o array [1, 2, 3, 4]
-Faça uma chamada para a função myRemoveWithoutCopy e verifique se o array passado por parâmetro sofreu alterações
-Verifique se a chamada myRemoveWithoutCopy([1, 2, 3, 4], 5) retorna o array esperado
-*/
+describe('A função recebe um array e retorna o mesmo sem o elemento item caso ele exista no array', () => {
+  it('Verifique se a chamada retorna o array esperado', () => {
+    const expected_result = [1, 2, 4];
+    expect(myRemoveWithoutCopy([1, 2, 3, 4], 3)).toEqual(expected_result);
+  });
+
+  it('Verifique se a mesma chamada não retorna o array [1, 2, 3, 4]', () => {
+    const not_expected_result = [1, 2, 3, 4];
+    expect(myRemoveWithoutCopy([1, 2, 3, 4], 3)).not.toEqual(not_expected_result);
+  });
+
+  it('Faça uma chamada para a funçãoe verifique se o array passado por parâmetro sofreu alterações', () => {
+    const arr = [1, 2, 3, 4];
+    myRemoveWithoutCopy(arr, 3);
+
+    const expected_result = [1, 2, 4];
+    expect(arr).toEqual(expected_result);
+  });
+
+  it('Verifique se a chamada myRemoveWithoutCopy([1, 2, 3, 4], 5) retorna o array esperado', () => {
+    const expected_result = [1, 2, 3, 4];
+    expect(myRemoveWithoutCopy([1, 2, 3, 4], 5)).toEqual(expected_result);
+  });
+});
