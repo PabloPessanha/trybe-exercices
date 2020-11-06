f1428c401ce99ec67ca18907637e2c63a2f80d3a
Pablo <pablopessanh@gmail>
2020-11-06 13:11:16 -0300

    refactored App code

--- block-11/react-intro/src/App.js
+++ block-11/react-intro/src/App.js
@@ -2,15 +2,11 @@ import './App.css';
 
 function App() {
   const compromises = ['Limpar mesa de jantar', 'Receber amigos', 'Assistir filme juntos'];
-  const tasks = (value) => {
-    return (<li>{value}</li>);
-  };
+  const tasks = (value) => <li>{value}</li>;
 
   return (
     <div>
-      <ul>
-        {compromises.map(task => tasks(task))}
-      </ul>
+      <ul>{compromises.map((task) => tasks(task))}</ul>
     </div>
   );
 }
