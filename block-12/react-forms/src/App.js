8c439fb9bfb4e530ed79a8ad7073f9879faacb15
Pablo <pablopessanh@gmail>
2020-11-11 13:27:34 -0300

    Add files for React Forms day lecture

---
+++ block-12/react-forms/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Forms from './components/Forms';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <Forms />
+    </div>
+  );
+}
+
+export default App;
